9c675c36365a55ddc313aaa46673fa8c035cac9e
Danil Mazurkin <mazurkin.daniel@gmail.com>
2022-07-27 15:01:15 +1000

    Прикрутил alembic для миграций

---
+++ privatenotik/alembic/versions/b36e53d5b282_добавиль_модель_заметки.py
@@ -0,0 +1,24 @@
+"""Добавиль модель заметки
+
+Revision ID: b36e53d5b282
+Revises: 
+Create Date: 2022-07-27 14:57:46.679787
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b36e53d5b282'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
